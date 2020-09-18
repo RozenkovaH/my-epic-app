da105f0e32689a238e22e310c80fde1db6dc7a42
VladislavMaksimov <workmaximov@yandex.ru>
2020-09-18 14:16:53 +0500

    Task replacing other elements when rendered.

--- src/Components/Workspace/Workspace.jsx
+++ src/Components/Workspace/Workspace.jsx
@@ -1,18 +1,28 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import ReactDOM, { render } from 'react-dom'
 import styles from './WorkspaceStyles.module.css'
 import WorkspaceHeader from '../WorkspaceHeader/WorkspaceHeader'
 import Task from '../Task/Task'
 import SearchBar from '../SearchBar/SearchBar'
 
 const Workspace = () => {
+
+  const renderTask = () => {
+    const container = document.getElementsByClassName(styles.workspace)[0];
+    console.log(container)
+    render(
+      <Task/>,
+      container
+    )
+  }
+
   return (
-    <form>
+    <div>
       <div className={styles.workspace}>
-        <WorkspaceHeader/>
+        <WorkspaceHeader onClick={renderTask}/>
         <SearchBar/>
-        <Task/>
       </div>
-    </form>
+    </div>
   )
 }
 export default Workspace

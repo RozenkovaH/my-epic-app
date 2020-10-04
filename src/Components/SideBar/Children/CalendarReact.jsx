ab9a54118050fd358132e4db8d4986912e9ccd8f
VladislavMaksimov <workmaximov@yandex.ru>
2020-10-05 01:15:31 +0500

    Исправил косяки.

--- src/Components/SideBar/Children/CalendarReact.jsx
+++ src/Components/SideBar/Children/CalendarReact.jsx
@@ -3,11 +3,10 @@ import styles from './CalendarReactStyles.module.css'
 import 'react-calendar/dist/Calendar.css'
 import Calendar from 'react-calendar'
 
-const CalendarReact = () => {
+const CalendarReact = (props) => {
   useEffect(() => {
     const now = document.getElementsByClassName('react-calendar__tile--now')[0];
     now.style.background = '#0073F7';
-    now.style.borderRadius = '50%';
     now.style.color = 'white';
   })
 

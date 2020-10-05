75ceb33264e19d6fb96cd681cce03a8ae3292dbf
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-10-05 23:04:43 +0500

    Styled auth button

--- src/Components/Signin/Signin.jsx
+++ src/Components/Signin/Signin.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './SignInStyles.module.css'
+import styles from './SigninStyles.module.css'
 
 const SignIn = props => {
   return (
@@ -14,7 +14,15 @@ const SignIn = props => {
           <h1>THE BEST PLANNER EVER</h1>
         </div>
         <form action='https://garage-best-team-ever.tk/google-auth'>
-          <button>Войти через google</button>
+          <button className={styles.googleBtn}>
+            <svg width="48" height="53" viewBox="0 0 48 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="48" height="53" fill="white" />
+              <path d="M15.427 29.5251L14.4176 33.2935L10.728 33.3715C9.62543 31.3264 9 28.9865 9 26.5C9 24.0955 9.58476 21.8281 10.6213 19.8316L13.9068 20.4338L15.3457 23.6988C15.0445 24.5768 14.8804 25.5193 14.8804 26.5C14.8805 27.5644 15.0733 28.5842 15.427 29.5251Z" fill="#FBBB00" />
+              <path d="M37.7466 23.7912C37.9132 24.6684 38 25.5742 38 26.5C38 27.5381 37.8909 28.5507 37.6829 29.5275C36.9771 32.8514 35.1327 35.7537 32.5777 37.8076L32.5769 37.8068L28.4396 37.5957L27.8541 33.9404C29.5494 32.9462 30.8744 31.3902 31.5723 29.5275H23.8188V23.7912H31.6854H37.7466Z" fill="#518EF8" />
+              <path d="M32.5768 37.8068L32.5776 37.8076C30.0927 39.8049 26.9362 41 23.5 41C17.978 41 13.1771 37.9136 10.728 33.3716L15.427 29.5251C16.6515 32.7932 19.8041 35.1196 23.5 35.1196C25.0886 35.1196 26.5769 34.6901 27.854 33.9404L32.5768 37.8068Z" fill="#28B446" />
+              <path d="M32.7553 15.3382L28.0579 19.1838C26.7362 18.3577 25.1738 17.8804 23.5 17.8804C19.7204 17.8804 16.5089 20.3135 15.3457 23.6988L10.6221 19.8316H10.6213C13.0345 15.1788 17.896 12 23.5 12C27.0182 12 30.2441 13.2532 32.7553 15.3382Z" fill="#F14336" />
+            </svg>
+          <p>Войти через Google</p></button>
         </form>
       </div>
       <svg className={styles.abstractShapes} position="fixed" top="0" bottom="0" left="0" right="0" viewBox="0 0 1440 933" fill="none" xmlns="http://www.w3.org/2000/svg">

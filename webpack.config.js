59ed7fac3bc87b8f3385bab00ac66aa5aeaf9de4
Christina Rozenkova <45661962+RozenkovaH@users.noreply.github.com>
2020-10-06 13:55:57 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,7 @@ module.exports = (env, argv) => ({
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
     port: 9000,
     host: '0.0.0.0',
     hot: true

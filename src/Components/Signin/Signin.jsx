eebe35ee50eff3b9349acf6c8df0731526f5747d
VladislavMaksimov <workmaximov@yandex.ru>
2020-10-06 17:09:10 +0500

    Убрал скролл.

--- src/Components/Signin/Signin.jsx
+++ src/Components/Signin/Signin.jsx
@@ -3,7 +3,7 @@ import styles from './SigninStyles.module.css'
 
 const SignIn = props => {
   return (
-    <div>
+    <div className={styles.page}>
       <div className={styles.container}>
         <div className={styles.logoAndTitle}>
           <svg width="67" height="67" viewBox="0 0 67 67" fill="none" xmlns="http://www.w3.org/2000/svg">
